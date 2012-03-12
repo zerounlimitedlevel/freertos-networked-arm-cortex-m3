e3ac130da453027a62f52ea04be15783fdac8df1
qarce76@gmail.com <qarce76@gmail.com@23083460-f366-6d7e-27b5-8e712da71019>
2012-03-12 04:16:36 +0000

    check in StellarisWare example code that came with the dev board

---
+++ external/StellarisWare/boards/ek-lm3s8962/boot_eth/rvmdk/bl_packet.d
@@ -0,0 +1,7 @@
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader\bl_packet.c
+.\rvmdk\bl_packet.o: .\bl_config.h
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader/bl_commands.h
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader/bl_i2c.h
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader/bl_packet.h
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader/bl_ssi.h
+.\rvmdk\bl_packet.o: ..\..\..\boot_loader/bl_uart.h
---
+++ external/StellarisWare/boards/ek-lm3s8962/boot_serial/rvmdk/bl_ssi.d
@@ -0,0 +1,8 @@
+.\rvmdk\bl_ssi.o: ..\..\..\boot_loader\bl_ssi.c
+.\rvmdk\bl_ssi.o: ..\..\..\inc/hw_gpio.h
+.\rvmdk\bl_ssi.o: ..\..\..\inc/hw_memmap.h
+.\rvmdk\bl_ssi.o: ..\..\..\inc/hw_ssi.h
+.\rvmdk\bl_ssi.o: ..\..\..\inc/hw_sysctl.h
+.\rvmdk\bl_ssi.o: ..\..\..\inc/hw_types.h
+.\rvmdk\bl_ssi.o: .\bl_config.h
+.\rvmdk\bl_ssi.o: ..\..\..\boot_loader/bl_ssi.h
