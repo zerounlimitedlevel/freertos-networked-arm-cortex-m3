b1b2e0c1b39391ef66b337b89df62c7bf88fac62
qarce76 <qarce76@23083460-f366-6d7e-27b5-8e712da71019>
2012-04-02 23:47:45 +0000

    copy to start work

---
+++ scratch/mydev/boards/ek-lm3s8962/boot_serial/rvmdk/bl_enet.d
@@ -0,0 +1,14 @@
+.\rvmdk\bl_enet.o: ..\..\..\boot_loader\bl_enet.c
+.\rvmdk\bl_enet.o: C:\Keil\\ARM\RV31\INC\string.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_ethernet.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_flash.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_gpio.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_memmap.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_nvic.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_sysctl.h
+.\rvmdk\bl_enet.o: ..\..\..\inc/hw_types.h
+.\rvmdk\bl_enet.o: .\bl_config.h
+.\rvmdk\bl_enet.o: ..\..\..\boot_loader/bl_decrypt.h
+.\rvmdk\bl_enet.o: ..\..\..\boot_loader/bl_flash.h
+.\rvmdk\bl_enet.o: ..\..\..\driverlib/rom.h
+.\rvmdk\bl_enet.o: ..\..\..\boot_loader/bl_hooks.h
---
+++ scratch/mydev/boards/ek-lm3s8962/boot_serial/rvmdk/bl_i2c.d
@@ -0,0 +1,8 @@
+.\rvmdk\bl_i2c.o: ..\..\..\boot_loader\bl_i2c.c
+.\rvmdk\bl_i2c.o: ..\..\..\inc/hw_gpio.h
+.\rvmdk\bl_i2c.o: ..\..\..\inc/hw_i2c.h
+.\rvmdk\bl_i2c.o: ..\..\..\inc/hw_memmap.h
+.\rvmdk\bl_i2c.o: ..\..\..\inc/hw_sysctl.h
+.\rvmdk\bl_i2c.o: ..\..\..\inc/hw_types.h
+.\rvmdk\bl_i2c.o: .\bl_config.h
+.\rvmdk\bl_i2c.o: ..\..\..\boot_loader/bl_i2c.h
